23c87be34699179b972e14d31b4466b0d51d9c52
FaZeJose <121144907+FaZeJose@users.noreply.github.com>
2022-12-23 11:05:31 -0500

    Add files via upload

---
+++ Streamlit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import streamlit as st
+
+st.write("""    
+# Encuesta de platos de comida 
+
+En esta app mostraremos datos estadísticos de una encuesta de platos de comida
+
+""")
+
